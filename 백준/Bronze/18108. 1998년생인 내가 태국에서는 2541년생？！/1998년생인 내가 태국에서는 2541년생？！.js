9c3956e4cb1c0c497d42458786f5ca576949600a
Jungwoo Jo <fixingwoo@gmail.com>
2025-02-19 11:13:21 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 96 ms, Memory: 9600 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+// const input = fs.readFileSync('test.txt').toString().trim().split('\n');
+
+function func(arr) {
+  console.log(arr[0] - 543);
+}
+
+func(input);
