e61a410692168707277f692dbe49bb76947cce10
Jungwoo Jo <fixingwoo@gmail.com>
2025-02-19 11:30:47 +0900

    [Bronze V] Title: 나머지, Time: 96 ms, Memory: 9596 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+// const input = fs.readFileSync('test.txt').toString().trim().split('\n');
+
+function func(arr) {
+  const [a, b, c] = arr[0].split(' ').map(Number);
+
+  console.log((a + b) % c);
+  console.log(((a % c) + (b % c)) % c);
+  console.log((a * b) % c);
+  console.log(((a % c) * (b % c)) % c);
+}
+
+func(input);
