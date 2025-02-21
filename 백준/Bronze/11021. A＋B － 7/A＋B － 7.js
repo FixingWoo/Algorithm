8f04fb3d348476986e6dad44008fab5cb75e4df5
Jungwoo Jo <fixingwoo@gmail.com>
2025-02-21 13:35:39 +0900

    [Bronze V] Title: A+B - 7, Time: 100 ms, Memory: 9528 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+for (let i = 1; i < input.length; i++) {
+  const [a, b] = input[i].split(' ').map(Number);
+  console.log(`Case #${i}: ${a + b}`);
+}
