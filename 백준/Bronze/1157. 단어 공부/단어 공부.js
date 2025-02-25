8c5264d6bed8fd6aa1442693606787529f32b58a
Jungwoo Jo <fixingwoo@gmail.com>
2025-02-25 17:15:44 +0900

    Update 단어 공부.js

--- 백준/Bronze/1157. 단어 공부/단어 공부.js
+++ 백준/Bronze/1157. 단어 공부/단어 공부.js
@@ -27,3 +27,18 @@ for (let key in frequncy) {
 }
 
 console.log(arr.length > 1 ? '?' : arr[0]);
+
+
+// 개선한 코드, reduce 활용하자...
+const frequncy = input
+  .toUpperCase()
+  .split('')
+  .reduce((acc, char) => {
+    acc[char] = (acc[char] || 0) + 1;
+    return acc;
+  }, {});
+
+const max = Math.max(...Object.values(frequncy));
+const arr = Object.keys(frequncy).filter((key) => frequncy[key] === max);
+
+console.log(arr.length > 1 ? '?' : arr[0]);
