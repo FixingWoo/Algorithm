dcf70b7c58d5b3cf85dc63091a1033c7d8746689
Jungwoo Jo <fixingwoo@gmail.com>
2025-02-26 09:31:24 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 96 ms, Memory: 9432 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const N = Number(input[0]);
+let count = N;
+
+for (let i = 1; i <= N; i++) {
+  word = input[i];
+  const seen = new Set();
+  let prev = '';
+
+  for (let char of word) {
+    if (seen.has(char) && prev !== char) {
+      count--;
+      break;
+    }
+
+    seen.add(char);
+    prev = char;
+  }
+}
+
+console.log(count);
