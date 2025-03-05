4aae37bb13146db343bc35aab805962654e0e27b
Jungwoo Jo <fixingwoo@gmail.com>
2025-03-05 13:44:54 +0900

    Update 중앙 이동 알고리즘.js

--- 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.js
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.js
@@ -3,3 +3,7 @@ const input = fs.readFileSync('/dev/stdin').toString().trim();
 
 const result = (2 ** input + 1) ** 2;
 console.log(result);
+
+// 한 변의 사각형 > 2의 제곱
+// 한 변의 꼭짓점 > 한 변의 사각형 + 1
+// 총 꼭짓점 > 한 변의 꼭짓점의 제곱
