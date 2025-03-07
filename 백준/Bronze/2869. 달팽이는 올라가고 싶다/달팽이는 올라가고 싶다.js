00fc0958e25d2c045f5c4882c4095818063be2dd
Jungwoo Jo <fixingwoo@gmail.com>
2025-03-07 09:41:33 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 92 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim();
+
+const [A, B, V] = input.split(' ').map(Number);
+
+const days = Math.ceil((V - A) / (A - B) + 1);
+console.log(days);
